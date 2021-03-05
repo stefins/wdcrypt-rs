6f6f62f59412f5be76c198cda299b9a28f494950
Stef <sstefin@bk.ru>
2021-03-05 11:21:47 -0500

    Added full file encryption and cli clap

--- src/file_utils.rs
+++ src/file_utils.rs
@@ -55,15 +55,18 @@ pub fn encrypt_file(fname: &str, key: &String) -> Result<(), Error> {
     match fs::read(&fname) {
         Ok(content) => match File::create(&fname) {
             Ok(mut file) => {
-                match file.write(encryption::encrypt_to_cipher(&key, &*content).as_bytes()) {
-                    Ok(_) => Ok(()),
-                    Err(err) => Err(err),
-                }
+                println!("Encrypting {}", &fname);
+                file.write(encryption::encrypt_to_cipher(&key, &*content).as_bytes())
+                    .expect("Cannot write to file");
+                println!("Encrypted {}", &fname);
             }
-            Err(err) => Err(err),
+            Err(err) => {
+                return Err(err);
+            }
         },
-        Err(err) => Err(err),
+        Err(err) => return Err(err),
     }
+    Ok(())
 }
 
 pub fn decrypt_file(fname: &str, key: &String) -> Result<(), Error> {
@@ -78,13 +81,78 @@ pub fn decrypt_file(fname: &str, key: &String) -> Result<(), Error> {
         .expect("Cannot read the file");
     match File::create(&fname) {
         Ok(mut out_file) => {
+            println!("Decrypting {}", &fname);
             out_file
                 .write(&*encryption::decrypt_to_normal(&key, &encrypted_content))
                 .expect("Cannot write to file");
+            println!("Decrypted {}", &fname);
         }
         Err(err) => {
             return Err(err);
         }
     }
+    Ok(())
+}
+
+pub fn encrypt_all_files() -> Result<(), Error> {
+    let (tx, rx) = mpsc::channel();
+    match fs::read_dir(".") {
+        Ok(paths) => {
+            let key = fernet::Fernet::generate_key();
+            encryption::write_fernet_key_to_file(&key);
+            for path in paths {
+                let key = key.clone();
+                match path {
+                    Ok(p) => {
+                        let tx = tx.clone();
+                        if p.path().display().to_string() != "./.secret.key" {
+                            thread::spawn(move || {
+                                encrypt_file(&p.path().display().to_string(), &key).unwrap();
+                                tx.send(1).unwrap();
+                            });
+                        }
+                    }
+                    Err(_) => {}
+                }
+            }
+        }
+        Err(err) => {
+            return Err(err);
+        }
+    }
+    drop(tx);
+    for _ in rx {}
+    Ok(())
+}
+
+pub fn decrypt_all_files() -> Result<(), Error> {
+    let (tx, rx) = mpsc::channel();
+    match fs::read_dir(".") {
+        Ok(paths) => {
+            let key = encryption::read_fernet_key_from_file();
+            for path in paths {
+                let key = key.clone();
+                match path {
+                    Ok(p) => {
+                        let tx = tx.clone();
+                        if p.path().display().to_string() != "./.secret.key" {
+                            thread::spawn(move || {
+                                decrypt_file(&p.path().display().to_string(), &key).unwrap();
+                                tx.send(1).unwrap();
+                            });
+                        }
+                    }
+                    Err(err) => {
+                        return Err(err);
+                    }
+                }
+            }
+        }
+        Err(err) => {
+            return Err(err);
+        }
+    }
+    drop(tx);
+    for _ in rx {}
     Ok(())
 }
